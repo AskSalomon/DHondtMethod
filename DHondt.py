369d281cb2eaadef1e0387d807b07092efc95864
AskSalomon <63118398+AskSalomon@users.noreply.github.com>
2020-04-03 19:46:40 +0200

    Trying branches out for the first time (#1)

--- DHondt.py
+++ DHondt.py
@@ -6,3 +6,8 @@ your_age = input()
 
 print("Hello, " + your_name + " why have you chosen to say alive for all of " + your_age + "arent you dragging your heels a little bit?")
 
+print("\n I am sorry, if you want to insult me, type your insult now ")
+
+insult = input()
+
+print("That is very fair, I am indeed an " + insult)
