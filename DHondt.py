44a42f01936d5818bc21ed23e8462c91c8a3f2bd
AskSalomon <salomon.ask@gmail.com>
2020-04-03 18:32:47 +0100

    Ask trying to add his first stuff to git

--- DHondt.py
+++ DHondt.py
@@ -1,3 +1,8 @@
 your_name = input()
 
 print(your_name)
+
+your_age = input()
+
+print("Hello, " + your_name + " why have you chosen to say alive for all of " + your_age + "arent you dragging your heels a little bit?")
+
