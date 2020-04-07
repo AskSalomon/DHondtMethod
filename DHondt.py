c1fb71ea25e5fb0a31ea70e1b4a654966b795187
AskSalomon <salomon.ask@gmail.com>
2020-04-07 11:08:50 +0100

    considering all the variables that this thing could have, will scale back to make simpler model

--- DHondt.py
+++ DHondt.py
@@ -5,3 +5,19 @@ numberOfParties = input()
 ###  2. Call the input() as many times as you have parties
 ###  3. Print out the information you have gotten this way.
 ###  4. (optional) save the vote tally to a file.
+
+
+print("To add number of seats press 's', to add a party press 'p' to add votes to a party press 'v' ")
+print("To see current tally press 't' to run election type 'democracy?'")
+
+def add_party
+
+def add_seats
+
+def seats_allocated
+
+def votes_total
+#basic d'hondt quotient = votes_total divided by seats_allocated + 1
+def quotient
+
+menu()
